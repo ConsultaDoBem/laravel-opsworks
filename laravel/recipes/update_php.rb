71c0a9d9860152cd5fe44747422b39fe98a9ecbf
Rafael Diniz <rafaelshekinah@gmail.com>
2015-09-30 19:18:17 -0300

    Update PHP

--- laravel/recipes/update_php.rb
+++ laravel/recipes/update_php.rb
@@ -1,14 +1,32 @@
 case node[:platform]
 when "rhel", "fedora", "suse", "centos", "amazon"
-  execute "yum remove php*" do
-    ignore_failure true
+  # uninstall php
+  script "uninstall_php" do
+    interpreter "bash"
+    user "root"
+    cwd "/"
+    code <<-EOH
+    yum remove php*
+    EOH
   end
 
-  execute "rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm" do
-    ignore_failure true
+  # install yum repo
+  script "install_yum_repo" do
+    interpreter "bash"
+    user "root"
+    cwd "/"
+    code <<-EOH
+    rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
+    EOH
   end
 
-  execute "yum install php54w. php54w-cli php54w-common php54w-gd php54w-ldap php54w-mbstring php54w-mcrypt php54w-mysql php54w-pdo" do
-    ignore_failure true
+  # install php54
+  script "install_php54" do
+    interpreter "bash"
+    user "root"
+    cwd "/"
+    code <<-EOH
+    yum install php54w php54w-cli php54w-common php54w-gd php54w-ldap php54w-mbstring php54w-mcrypt php54w-mysql php54w-pdo
+    EOH
   end
 end
