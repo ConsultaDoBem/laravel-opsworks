f949abb151263b56760957eeede6f8bd3b8bf36a
Rafael Diniz <rafaelshekinah@gmail.com>
2015-10-15 20:39:09 -0300

    Add RW permissions to img folder

---
+++ laravel/recipes/image_permissions.rb
@@ -0,0 +1,10 @@
+node[:deploy].each do |application, deploy|
+  script "image_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    chown -R www-data:www-data public/img
+    EOH
+  end
+end
