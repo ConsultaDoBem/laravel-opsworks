71d3ddbf1328d7986ccd932e30fdde347c89d6de
Rafael Diniz <rafaelshekinah@gmail.com>
2015-12-30 15:22:18 -0200

    Migration and cron

---
+++ laravel/recipes/cron.rb
@@ -0,0 +1,26 @@
+node[:deploy].each do |application, deploy|
+  script "file_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+
+# Process queue db - Every 2 minutes
+*/2 * * * * php /srv/www/monaco/current/artisan cdb:queue-process --env=production
+
+# Process all payments - Every 2 minutes
+*/2 * * * * php /srv/www/monaco/current/artisan cdb:payer-process --env=production
+
+# Process bank slips
+0 */1 * * * php /srv/www/monaco/current/artisan cdb:billet-status --env=production
+
+# Process update balance  - 2am every day
+0 2 * * * php /srv/www/monaco/current/artisan cdb:balance-process --env=production
+
+# Search Indexing Full - 3am every day
+0 3 * * * php /srv/www/monaco/current/artisan cdb:search-indexing --env=production
+
+
+    EOH
+  end
+end
