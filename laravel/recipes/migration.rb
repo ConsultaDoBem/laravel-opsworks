4e924c2c9d567bf50cecce20f8bd3b13645544f5
Rafael Diniz <rafaelshekinah@gmail.com>
2016-01-07 10:52:04 -0200

    Fix migration to run only on monaco

--- laravel/recipes/migration.rb
+++ laravel/recipes/migration.rb
@@ -1,10 +1,14 @@
-node[:deploy].each do |application, deploy|
-  script "migration" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    php artisan migrate --env=qa
-    EOH
+case node[:application]
+when "monaco"
+
+  node[:deploy].each do |application, deploy|
+    script "migration" do
+      interpreter "bash"
+      user "root"
+      cwd "#{deploy[:deploy_to]}/current"
+      code <<-EOH
+      php artisan migrate --env=qa
+      EOH
+    end
   end
 end
