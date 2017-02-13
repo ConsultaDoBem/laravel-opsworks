d76a55077cc62dbb8a254eba3b1183fe3872d39c
Eduardo Alencar <ealencar10@gmail.com>
2017-02-13 16:22:39 -0200

    Retirado mudança de permissão na storage

--- laravel/recipes/storage_permissions.rb
+++ laravel/recipes/storage_permissions.rb
@@ -4,8 +4,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    chown -R www-data:www-data app/storage
-    sudo chmod -R 777 app/storage
+    echo $USER
     EOH
   end
 end
