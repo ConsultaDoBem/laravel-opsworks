562b6a21bb4d817d5caa96950992b07adabc36d4
Eduardo Alencar <ealencar10@gmail.com>
2017-02-16 09:33:55 -0200

    Forçando instalação das dependencias do composer

--- laravel/recipes/storage_permissions-prd.rb
+++ laravel/recipes/storage_permissions-prd.rb
@@ -5,6 +5,7 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     chown -R www-data:www-data app/storage
+    php composer.phar install --no-dev --no-interaction --prefer-dist
     find app/storage -type d -exec chmod 777 {} \;
     find app/storage -type f -exec chmod 777 {} \;
     EOH
